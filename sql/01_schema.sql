f44c59b7a816cecacb637e32531d4ec7d1b2aba5
baypogiMD <49798713+baypogiMD@users.noreply.github.com>
2026-02-15 15:58:44 +0800

    Create 01_schema.sql

---
+++ sql/01_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE urinary_biomarkers (
+    patient_id INTEGER PRIMARY KEY,
+    age INTEGER,
+    sex TEXT,
+    creatinine REAL,
+    lyve1 REAL,
+    reg1b REAL,
+    tff1 REAL,
+    diagnosis TEXT
+);
